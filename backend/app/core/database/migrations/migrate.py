f4b6ea1701c2e50b05e1717b2a59422851309db3
Fasih Hussain <fasihhussain00@gmail.com>
2025-03-09 18:08:09 +0500

    chore: migration fixed

--- backend/app/core/database/migrations/migrate.py
+++ backend/app/core/database/migrations/migrate.py
@@ -6,14 +6,10 @@ from colorama import Fore, Style, init
 init(autoreset=True)
 
 dsn = urlparse(settings.CLICKHOUSE_DSN)
-db_host = dsn.hostname
-db_user = dsn.username
-db_password = dsn.password
 db_name = dsn.path.lstrip("/")
 migrations_home = "app/core/database/migrations"
 
-cluster = ClickhouseCluster(db_host, db_user, db_password)
-
+cluster = ClickhouseCluster(dsn.hostname, dsn.username, dsn.password)
 migrations = cluster.migrate(
     db_name,
     migrations_home,
@@ -23,15 +19,13 @@ migrations = cluster.migrate(
 )
 
 print(
-    f"{Fore.CYAN}{Style.BRIGHT}Running migrations for database: {db_name}{Style.RESET_ALL}"
+    f"{Fore.CYAN}{Style.BRIGHT}Running migrations for {db_name}{Style.RESET_ALL}"
 )
-print(f"{Fore.YELLOW}Host: {db_host}, User: {db_user}{Style.RESET_ALL}")
-
 for migration in migrations:
     print(
         f"{Fore.GREEN}Migration {migration.version} with:{Style.RESET_ALL}\n{Fore.LIGHTBLACK_EX}{migration.script}{Style.RESET_ALL}"
     )
 
 print(
-    f"{Fore.MAGENTA}{Style.BRIGHT}\nMigration process completed successfully!{Style.RESET_ALL}"
+    f"{Fore.MAGENTA}{Style.BRIGHT}\n{len(migrations)} Migration Applied\nMigration process completed successfully!{Style.RESET_ALL}"
 )
